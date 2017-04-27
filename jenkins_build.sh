652bfad4ab6f36262eb53abd16668a430365d522
hernad <hernad@bring.out.ba>
2017-04-27 17:26:39 +0200

    ARCH

--- jenkins_build.sh
+++ jenkins_build.sh
@@ -6,12 +6,19 @@ if [ ! -f packer.zip ] ; then
   unzip packer.zip
 fi
 
-# ako je na silu prosli put prekinuto 
-VBoxManage controlvm ubuntu-16.04-i386 poweroff
-VBoxManage unregistervm ubuntu-16.04-i386 --delete
+ARCH=i386
+
+rm -rf packer-ubuntu-16.04-${ARCH}-virtualbox
+# ako je na silu prosli put prekinuto
+VBoxManage controlvm ubuntu-16.04-${ARCH} poweroff
+VBoxManage unregistervm ubuntu-16.04-${ARCH} --delete
+VBoxManage unregistervm ubuntu-16.04-${ARCH} --delete
+
+rm -rf "/home/docker/VirtualBox VMs/ubuntu-16.04-${ARCH}"
+
 
 chmod +x packer
-./packer build -var 'headless=true'  -only=virtualbox-iso ubuntu-16.04-i386.json
+./packer build -var 'headless=true'  -only=virtualbox-iso ubuntu-16.04-${ARCH}.json
 
-mv builds/ubuntu-16.04-i386.virtualbox.box ubuntu_16.04-i386_$(date +"%Y-%d-%m").box
+mv builds/ubuntu-16.04-i386.virtualbox.box ubuntu_16.04-${ARCH}_$(date +"%Y-%d-%m").box
 
